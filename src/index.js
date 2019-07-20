030c4bcded66cb5fdc2d0e594857cdf86edd935a
Your Name <you@example.com>
2019-07-20 21:17:27 +0300

    add project

---
+++ src/index.js
@@ -0,0 +1,5 @@
+import { weather } from './modules/changeWeather'
+import { select } from './modules/select'
+
+weather();
+select();
---
+++ src/modules/select.js
@@ -0,0 +1,51 @@
+export let select = function(){
+    let selectWrap = document.querySelectorAll('.custom-select');
+
+    function createFirstSelect() {
+        for (let i = 0; i < selectWrap.length; i++) {
+            let newList = createSelectList(selectWrap[i]),
+                defaultSelect = selectWrap[i].querySelectorAll('select')[0];
+                let dsds = document.createElement('div');
+                dsds.classList.add('current-select');
+                selectWrap[i].insertBefore(dsds, newList).innerHTML = 'не написано';
+                
+                selectWrap[i].addEventListener('click', function(e) {
+                    let selectItem = selectWrap[i].querySelectorAll('.select-item'),
+                        target = e.target;            
+                    newList.classList.add('active');
+
+                    if ( selectItem.length > 0 ) {
+                        removeSelectItem(selectItem);
+                        newList.classList.remove('active');
+                        dsds.innerHTML = target.innerHTML;
+                    } else {
+                        for ( let i = 0; i < defaultSelect.length; i++ ) {
+                            let optionText = defaultSelect[i].innerHTML;
+                            createSelectItem(newList, optionText);
+                        }
+                    }
+                });
+        }
+    }
+    createFirstSelect();
+    
+    function createSelectList(par) {
+        let list = document.createElement('div');
+        list.classList.add('select-list');
+        return par.appendChild(list);
+    }
+
+    function createSelectItem(par_1, par_2) {
+        let item = document.createElement('div');
+        item.classList.add('select-item');
+        item.textContent = par_2;
+        par_1.appendChild(item);
+    }
+
+    function removeSelectItem(par) {
+        for (let i = 0; i < par.length; i++) {
+            par[i].parentNode.removeChild(par[i]); 
+        }
+    }
+
+}
